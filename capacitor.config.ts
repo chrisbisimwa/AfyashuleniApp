4fa0c586c50e895b84fd9816089f8a68f7c0cf5d
Christian BISIMWA <chrisbisimwa@outlook.fr>
2024-08-29 08:32:38 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AfyashuleniApp',
+  webDir: 'www'
+};
+
+export default config;
