907da7d708fdc08f83bda11ce9378c388e7ccb80
Christian BISIMWA <chrisbisimwa@outlook.fr>
2024-08-31 04:18:36 +0200

    login page

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  email = '';
+  password = '';
+
+  constructor(private authService: AuthService, private router:Router) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    const credentials = {"email": this.email, "password":this.password};
+    this.authService.login(credentials)
+      .subscribe(
+        () => {
+          // Connexion réussie, rediriger vers la page d'accueil
+          
+          this.router.navigate(['/home']);
+        },
+        (error) => {
+          // Gérer les erreurs de connexion
+          console.error('Erreur de connexion:', error);
+          // Afficher un message d'erreur à l'utilisateur
+        }
+      );
+  }
+
+  forgotPassword() {
+    this.router.navigate(['password-reset']);
+  }
+
+}
